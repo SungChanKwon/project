9823035c1efdb124f9d8c8aa2232641fd96140e9
SungChanKwon <kimhn1001@gmail.com>
2024-03-25 15:33:29 +0900

    Update pagingVO.java

--- src/main/java/com/ott/dto/pagingVO.java
+++ src/main/java/com/ott/dto/pagingVO.java
@@ -1,4 +1,4 @@
-package com.net.dto;
+package com.ott.dto;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -7,7 +7,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class pagingVO {
+public class PagingVO {
 	
 	private int page;
 	private int limit;
@@ -15,28 +15,23 @@ public class pagingVO {
 	private int startPage;
 	private int endPage;
 	private boolean prev, next;
-	
-	public pagingVO() {}
+	private int realEnd;
+	public PagingVO() {}
 		
-	public pagingVO(int page, int limit,int listCount) {
+	public PagingVO(int page, int limit,int listCount) {
 		this.page = page;
 		this.limit = limit;
 		this.listCount = listCount;
-		int realEnd;
-		realEnd = page%listCount != 0 ? (listCount/limit)+1:(listCount/limit);
+		realEnd = (int)Math.ceil(listCount/(double)limit);
 		startPage = ((int)(((double)page/limit+0.9))-1)*limit+1;
-		endPage = startPage+limit-1;
+		endPage =(int)Math.ceil(page*0.1)*10;
+		
 		
 		if(endPage > realEnd) {
 			endPage = realEnd;
 		}
-		
+	
 		prev = startPage > 1;
 		next = endPage < realEnd ;
-		
-		
 	}
-	
-	
-	
 }
