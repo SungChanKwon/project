fc3fc4cc92c5e384dd06024818f1ac921d564783
Leehyob <dlgyqls11@naver.com>
2024-03-25 14:47:44 +0900

    commit

---
+++ src/main/java/com/ott/controller/action/DeleteMemberAction.java
@@ -0,0 +1,28 @@
+package com.ott.controller.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ott.dao.MemberDAO;
+import com.ott.dto.MemberVO;
+
+public class DeleteMemberAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("넘어옴");
+		String userId = request.getParameter("userId");
+		
+		
+		System.out.println(userId);
+		MemberDAO.getInstance().deleteMember(userId);
+		
+		response.sendRedirect("OttServlet?command=member_info");
+		
+	}
+
+}
